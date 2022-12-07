349d54906bc4493698f7a8f2562cb5bc5f48dfca
Sari <sygw6457@gmail.com>
2022-12-07 21:59:00 +0200

    simulation

--- src/main/java/observer/Main.java
+++ src/main/java/observer/Main.java
@@ -3,9 +3,27 @@ package observer;
 
 public class Main
 {
+    public static void main(String [] args)
+    {
 
-// in our main we test all we want not in format of Junit :)
+        // in our main we test all
+        // we want not in format of Junit :)
+        GroupAdmin ga = new GroupAdmin();
+        ConcreteMember guest_0 = new ConcreteMember();
+        ConcreteMember guest_1 = new ConcreteMember();
+        ConcreteMember guest_2 = new ConcreteMember();
 
+        ga.register(guest_0);
+        ga.register(guest_1);
+        ga.register(guest_2);
+
+        ga.append("heyyyyyyyyyyyyyyy");
+
+        System.out.println(guest_0.get_usb());
+        System.out.println(guest_1.get_usb());
+        System.out.println(guest_2.get_usb());
+
+    }
 
 
 
