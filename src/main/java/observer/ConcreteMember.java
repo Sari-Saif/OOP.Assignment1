9ecc7761da43a7f6d18f9e81308a8c1a1d43b6d4
Sari <sygw6457@gmail.com>
2022-12-07 21:47:37 +0200

    implement class Member :)))

---
+++ src/main/java/observer/ConcreteMember.java
@@ -0,0 +1,17 @@
+package observer;
+
+public class ConcreteMember implements Member
+{
+    private UndoableStringBuilder _usb;
+
+    public ConcreteMember()
+    {
+        this._usb = new UndoableStringBuilder();
+    }
+
+    @Override
+    public void update(UndoableStringBuilder usb)
+    {
+        this._usb = usb;
+    }
+}
