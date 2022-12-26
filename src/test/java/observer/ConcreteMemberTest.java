789c3eaaa7cbb994110d0dfcb6a1eec02d00ca94
Sari <sygw6457@gmail.com>
2022-12-26 11:59:36 +0200

    loading :)) ->  ConcreteMemberTest.java

---
+++ src/test/java/observer/ConcreteMemberTest.java
@@ -0,0 +1,23 @@
+package observer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConcreteMemberTest {
+
+    @Test
+    void update()
+    {
+        ConcreteMember number_one = new ConcreteMember();
+
+
+
+
+
+    }
+
+    @Test
+    void get_usb() {
+    }
+}
